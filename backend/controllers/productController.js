d653f0fec5a440f2ffc3b0232f279c6c3ef535d4
pankajkorkalai@gmail.com <pankajkorkalai@gmail.com>
2024-08-11 12:00:48 +0530

    Searching, Filter, pagination Done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,7 +15,10 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+
+  const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
   const products = await apiFeature.query;
 
   console.log("Products:", products); // Log the products to check the response
@@ -40,6 +43,7 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+    productCount,
   });
 });
 
