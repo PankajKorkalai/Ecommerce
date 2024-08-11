9c3abf64ab328475f6ae24324498a46dd02d6fcc
pankajkorkalai@gmail.com <pankajkorkalai@gmail.com>
2024-08-11 11:04:45 +0530

    Searching Functionality Done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,80 +2,84 @@ const mongoose = require("mongoose");
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
-    const product = await Product.create(req.body);
-    res.status(201).json({
-        success: true,
-        product,
-    });
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
 });
 
 // Get All products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-    const products = await Product.find();
-    res.status(200).json({
-        success: true,
-        products,
-    });
+  const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+  const products = await apiFeature.query;
+
+  console.log("Products:", products); // Log the products to check the response
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
 });
 
 // Get Product Details
 exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return next(new ErrorHander("Product not found", 404));
-    }
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new ErrorHander("Product not found", 404));
+  }
 
-    
-    // if (!product) {
-    //     return next(new ErrorHander("Product not found", 404));
-    // }
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
+  }
 
-    res.status(200).json({
-        success: true,
-        product,
-    });
+  res.status(200).json({
+    success: true,
+    product,
+  });
 });
 
 // Update product -- Admin
 exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
-    let product = await Product.findById(req.params.id);
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return next(new ErrorHander("Product not found", 404));
-    }
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new ErrorHander("Product not found", 404));
+  }
 
-    // if (!product) {
-    //     return next(new ErrorHander("Product not found", 404));
-    // }
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
+  }
 
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-    });
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(200).json({
-        success: true,
-        product,
-    });
+  res.status(200).json({
+    success: true,
+    product,
+  });
 });
 
 // Delete Product
 exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        return next(new ErrorHander("Product not found", 404));
-    }
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return next(new ErrorHander("Product not found", 404));
+  }
 
-    // if (!product) {
-    //     return next(new ErrorHander("Product not found", 404));
-    // }
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
+  }
 
-    await product.deleteOne();
+  await product.deleteOne();
 
-    res.status(200).json({
-        success: true,
-        message: "Product Deleted Successfully",
-    });
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
+  });
 });
