9a1ba7b436e43f26669ab0c1ff7fa849ce48b7c3
pankajkorkalai@gmail.com <pankajkorkalai@gmail.com>
2024-08-12 20:10:54 +0530

    Email fuctionality done

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {registerUser, loginUser, logout} = require("../controllers/userController");
+const {registerUser, loginUser, logout, forgotPassword} = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
+router.route("/password/forgot").post(forgotPassword);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 
