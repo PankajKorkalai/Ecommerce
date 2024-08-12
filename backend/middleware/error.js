e09e1f1f6b964e4d493c614d31f06739eabc97f2
pankajkorkalai@gmail.com <pankajkorkalai@gmail.com>
2024-08-12 23:26:23 +0530

    login and signup backend complete moving to backend user routes apis

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -16,7 +16,23 @@ module.exports = (err, req, res, next) => {
         err.statusCode = 500; // Fallback to 500 Internal Server Error
     }
 
-    console.error(err); // Log the error for debugging
+    // Mongoose duplicate key error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+        const message = `Json Web Token is invalid, Try again `;
+        err = new ErrorHandler(message, 400);
+    }
+    
+    // JWT EXPIRE error
+    if (err.name === "TokenExpiredError") {
+        const message = `Json Web Token is Expired, Try again `;
+        err = new ErrorHandler(message, 400);
+    }
 
     res.status(err.statusCode).json({
         success: false,
