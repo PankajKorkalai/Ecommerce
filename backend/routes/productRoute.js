10bcdd20804bc247fb78b324afa2b89cc39f673b
pankajkorkalai@gmail.com <pankajkorkalai@gmail.com>
2024-08-13 21:19:46 +0530

    All User Routes Completed

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,17 +6,31 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  deleteReview,
+  getProductReviews,
 } = require("../controllers/productController");
-const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/products").get( getAllProducts);
-router.route("/products/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router.route("/products").get(getAllProducts);
+
 router
-  .route("/product/:id")
+  .route("/admin/products/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/admin/product/:id")
   .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
   .get(getProductDetails);
 
+router.route("/product/:id").get(getProductDetails);
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
+  
 module.exports = router;
